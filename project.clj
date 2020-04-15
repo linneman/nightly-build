04526e507ef6c6dcd4968d988c5fcc5bbeb009a3
Otto Linnemann <linneman@gmx.de>
2020-04-15 21:57:14 +0200

    v1.0.2
    Signed-off-by: Otto Linnemann <linneman@gmx.de>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bond "1.0.1"
+(defproject bond "1.0.2"
   :description "A Functional Programming Environment for the Automatic Creation of Software Release Candidates"
   :url "https://linneman.github.io/bond"
   :license {:name "GNU Lesser General Public License"
